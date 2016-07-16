9b27f3fbbeddae8624eb65c03276e6f8f02b22dc
Yusan Lin <yusan@psu.edu>
2016-07-16 17:56:58 -0400

    Added doc2vec

---
+++ doc2vec/doc2vec.py
@@ -0,0 +1,6 @@
+"""
+doc2vec.py
+@author: Yusan Lin
+@description: This program implements the Paragraph Vector proposed in 
+[1] Quoc Le, Tomas Mikolov, Distributed Representations of Sentences and Documents, https://cs.stanford.edu/~quocle/paragraph_vector.pdf
+"""
--- word2vec.py
+++ word2vec.py
@@ -21,10 +21,11 @@ from nltk.stem.porter import PorterStemmer
 from sklearn.cross_validation import train_test_split
 
 # Some constants that will be refer to later
-N_V   = 1000 # number of vocabularies
-N_H   = 50   # number of neurons in the hidden layer
-C     = 2    # context window size, meaning including C left words and C right words (2C + 1 in total)
-MIN_F = 100
+N_V      = 1000 # number of vocabularies
+N_H      = 50   # number of neurons in the hidden layer
+C        = 2    # context window size, meaning including C left words and C right words (2C + 1 in total)
+MIN_F    = 100
+MAX_ITER = 10   # maximum allowed training iterations
 
 path_data = "data/"
 
@@ -109,7 +110,8 @@ processed_sentences = 0.0
 accuracy = float("-inf")
 
 # Beacuse we don't want to try on the whole dataset yet, let's subsample it
-corpus_subsample = random.sample(corpus, 5000)
+n_samples = int(raw_input("How many sentences do you want to sub sample? "))
+corpus_subsample = random.sample(corpus, n_samples)
 
 #X = np.array()
 #y = np.array()
@@ -121,11 +123,14 @@ print "Constructing X and Y"
 for sentence in corpus_subsample:
 
 	print processed_sentences, "sentences processed"
+	print sentence
 
 	sentence_tmp = "<s> " * C + sentence + " <s>" * C
+	sentence_tmp = sentence_tmp.split()
 
 	# Everytime send in a context window
 	for i in range(C, len(sentence_tmp) - C + 1):
+		#print sentence_tmp[i]
 		x = np.zeros(N_V)
 
 		for j in range(i - C, i + C):
@@ -137,7 +142,7 @@ for sentence in corpus_subsample:
 					pass
 				x += tmp
 
-		x = np.array([ w/float(2 * C) for w in x]).reshape((1, N_V))
+		x = np.array([ w/float(2 * C) for w in x]).reshape((1, N_V))[0]
 
 		# the ith word is thus the target word for prediction
 		y = np.zeros(N_V)
@@ -145,7 +150,7 @@ for sentence in corpus_subsample:
 			y[vocab.index(sentence_tmp[i])] = 1
 		except ValueError:
 			pass
-		y = y.reshape((1, N_V))
+		y = y.reshape((1, N_V))[0]
 
 		X.append(list(x))
 		Y.append(list(y))
@@ -153,15 +158,16 @@ for sentence in corpus_subsample:
 	processed_sentences += 1
 
 X = np.array(X)
-y = np.array(y)
+Y = np.array(Y)
 
 # Split the X and Y into training and testing
 print "Splitting into training and testing"
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 42)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.33, random_state = 42)
 
 # Fit the model
 print "Fitting the model"
-while accuracy < 50:
+while accuracy < 50 or interations < MAX_ITER:
 	model.fit(X_train, y_train, verbose = 0)
 	accuracy = model.evaluate(X_test, y_test)
+	accuracy = accuracy[1]
 	print "Accuracy: ", accuracy, "%"
